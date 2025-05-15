7e500ce7553caa78d457206c117bf0de1609353a
d-Akkya <akhileshbamane26@gmail.com>
2025-05-15 11:13:38 +0530

    add cron job for health checks

--- configuration_management/services_health_check.sh
+++ configuration_management/services_health_check.sh
@@ -1,3 +1,8 @@
+LOG_DIR="/home/ubuntu/task-ci-cd/configuration_management/log/service_health"
+mkdir -p "$LOG_DIR"
+LOG_FILE="$LOG_DIR/health_$(date +'%Y-%m-%d_%H-%M').log"
+
+echo "Health Check - $(date)" > "$LOG_FILE"
 echo "Verifying services..."
 systemctl is-active --quiet docker && echo "Docker is running"
 systemctl is-active --quiet jenkins && echo "Jenkins is running"
